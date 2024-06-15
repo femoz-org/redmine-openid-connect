1a00b41ebd6f0ea1bf787383e0874d81b1fa4547
Jan Opravil <jan.opravil@femoz.cz>
2024-06-15 09:29:41 +0200

    chore: initial commint

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :femoz_openid_connect do
+  name 'Femoz Openid Connect plugin'
+  author 'femoz'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/femoz-org/redmine-openid-connect'
+  author_url 'https://github.com/femoz-org'
+end
